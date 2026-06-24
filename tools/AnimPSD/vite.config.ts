82c18255f62c751818f8b6d852ba4ae59607d35e
まぐじゃが <senamagu@gmail.com>
2026-06-25 08:36:25 +0900

    Update vite.config.ts

--- tools/AnimPSD/vite.config.ts
+++ tools/AnimPSD/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AnimPSD/', // リポジトリ名を設定
+  base: '/test.github.io/tools/AnimPSD/', // リポジトリ名を設定
 })
